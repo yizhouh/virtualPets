fe0a3c1c0c53ef16d2a98e8c5a0df447d3b18e6f
Kevin Huang <mail2yizhou@gmail.com>
2019-06-07 12:56:50 +1000

    write interface

--- IPets.cs
+++ IPets.cs
@@ -2,6 +2,24 @@ namespace cs_VirtualPets
 {
     interface IPets
     {
-        
+        string PetName { get; }
+        double RelationToMaster {get;}
+        string HappySound {get;}
+        string SadSound {get;}
+
+        //if master's relation amount to pet greater than 0.7, pet would respond, otherwise no respond.
+        bool RespondToCall(PetMaster master);
+
+        //respond by touching your pet should call happy sound
+        //increase the relationship
+        void RespondByTouch();
+
+        //complain when pet needs care
+        //decrease the relationship
+        void Complain();
+
+        //print the relationship amount
+        void PrintTheRelationship();
+
     }
 }
