4ba1ff1c9798441de8ef84d5cdd91dd0446833c9
Kevin Huang <mail2yizhou@gmail.com>
2019-06-07 12:05:28 +1000

    add comfort pets method

--- ToysAndSupplies.cs
+++ ToysAndSupplies.cs
@@ -1,7 +1,31 @@
+using System;
+
 namespace cs_VirtualPets
 {
     static class ToysAndSupplies
     {
-        
+        private static string[] Toys = new string[]{"toy1", "toy2", "toy3"};
+        private static string[] DressUps = new string[]{"dressup1", "dressup2", "dressup3"};
+        private static string[] Food = new string[]{"food1", "food2", "food3"};
+
+        public static void ComfortPets(IPets pet, string comfortOption)
+        {
+            if(comfortOption.ToLower() == "toy")
+            {
+                //do toys
+            }
+            else if(comfortOption.ToLower() == "dressup")
+            {
+                //do dressup
+            }
+            else if(comfortOption.ToLower() == "food")
+            {
+                //do food
+            }
+            else
+            {
+                //retrun
+            }
+        }
     }
 }
